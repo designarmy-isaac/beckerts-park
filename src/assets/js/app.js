4aad681734352b2b89b10bb2361c5155db9c6fe6
Isaac Morrier <isaacmorrier@gmail.com>
2019-03-21 11:39:07 -0400

    add leaf animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,63 @@ require('foundation-sites');
 
 
 $(document).foundation();
+$(document).ready(initPage);
+
+function initPage(){
+  
+  var $leaves = $('.leaf'),
+        bump = 10,
+        leaves = [];
+  
+  
+  function cacheLeafData() {
+    $leaves.each(function(i) {
+      var leaf = {};
+      leaf.e = $(this);
+      leaf.a = leaf.e.data("anchor");
+      leaf.r = leaf.e.data("rotate");
+      leaf.d = leaf.r + bump;
+      leaf.p = leaf.e.data("x"),
+      leaf.t = leaf.e.data("top"),
+      leaves[i] = leaf;
+//      console.log(i, leaf.animateIn);
+    },leaves); 
+  }
+  
+  function initLeaves() {
+    var i;
+    for (i = 0; i < leaves.length; i++) {
+      var leaf = leaves[i];
+      leaf.e.css({
+        "top": leaf.t,
+        "transform-origin": leaf.a + " center",
+        "transform": "rotate(" + leaf.r + "deg)"
+      });
+      if (leaf.a === "right") {
+        leaf.e.css("right", leaf.x);
+      } else {
+        leaf.e.css("left", leaf.x);
+      }
+      
+      leaves[i].e.hover(
+        function() {
+          animateLeafTo(leaves[$(this).index()].e, 2.25, {rotation: leaves[$(this).index()].r - bump, ease:Elastic.easeOut} );
+        },
+        function() {
+          animateLeafTo(leaves[$(this).index()].e, 2.25, {rotation: leaves[$(this).index()].r, ease:Elastic.easeOut} );
+        }
+      );
+    }
+  }
+  
+  function animateLeafTo(object, duration, parameters) {
+    TweenMax.to(object, duration, parameters);
+  }
+  
+  function write(m) {
+    console.log(m);
+  }
+   
+  cacheLeafData();
+  initLeaves();
+}
