3997b5dd8b71bd670d0993e82b17d0d3f92e7030
Isaac Morrier <isaacmorrier@gmail.com>
2019-06-11 15:20:35 -0400

    add loader and EHO logos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,11 +20,10 @@ $(document).foundation();
 $(document).ready(initPage);
 
 function initPage(){
-  
+ 
   var $leaves = $('.leaf'),
         bump = 5,
         leaves = [];
-  
   
   function cacheLeafData() {
     $leaves.each(function(i) {
@@ -76,10 +75,50 @@ function initPage(){
     TweenMax.to(object, duration, parameters);
   }
   
-  function write(m) {
-    console.log(m);
-  }
+//  function write(m) {
+//    console.log(m);
+//  }
    
   cacheLeafData();
   initLeaves();
+   
+  var width = 100,
+    perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
+    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
+    time = parseInt((EstimatedTime/1000)%60)*100;
+
+  // Loadbar Animation
+  $(".loadbar").animate({
+    width: width + "%"
+  }, time);
+
+  // Percentage Increment Animation
+  var PercentageID = $("#percent"),
+          start = 0,
+          end = 100,
+          durataion = time;
+          animateValue(PercentageID, start, end, durataion);
+
+  function animateValue(id, start, end, duration) {
+
+      var range = end - start,
+        current = start,
+        increment = end > start? 1 : -1,
+        stepTime = Math.abs(Math.floor(duration / range)),
+        obj = $(id);
+
+      var timer = setInterval(function() {
+          current += increment;
+          $(obj).text(current + "%");
+          if (current == end) {
+              clearInterval(timer);
+          }
+      }, stepTime);
+  }
+
+  // Fade out load bar when finished
+  setTimeout(function(){
+    $('.preloader-wrap').fadeOut(500);
+  }, time);
+  
 }
